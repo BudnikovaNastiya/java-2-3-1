d06a7bde1b760e227ed6d17c0f5e48caa28142dc
Anastasiya Budnikova <budnikova.a.p@gmail.com>
2021-06-02 16:31:53 +0300

    initial commit4

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,8 +1,5 @@
-package ru.netologi.bonus;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
